63cfbfc89d5e8b205e00ca362875629c1acfa8f2
Ely de Oliveira <wagnerag@itwm.fraunhofer.de>
2014-08-25 14:18:17 +0200

    Invalid route redirection.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  match '*path' => redirect('/'), via: :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
