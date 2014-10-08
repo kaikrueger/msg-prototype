9891e6eb95e2f284b2157847101a45429d450032
Ely de Oliveira <wagnerag@itwm.fraunhofer.de>
2014-10-08 13:47:27 +0200

    Client channel subscription.

--- client/websocket-rails-client-cpp/websocket-rails-client/client.cpp
+++ client/websocket-rails-client-cpp/websocket-rails-client/client.cpp
@@ -31,6 +31,24 @@ void post_failure(jsonxx::Object data) {
     std::cout << data.json() << std::endl;
 }
 
+void subscription_success(jsonxx::Object data) {
+
+    std::cout << "Function subscription_success called" << std::endl;
+    std::cout << data.json() << std::endl;
+}
+
+void subscription_failure(jsonxx::Object data) {
+
+    std::cout << "Function subscription_failure called" << std::endl;
+    std::cout << data.json() << std::endl;
+}
+
+void received_measurement(jsonxx::Object data) {
+
+    std::cout << "Function received_measurement called" << std::endl;
+    std::cout << data.json() << std::endl;
+}
+
 int main(int argc, const char* argv[]) {
 
     if (argc != 5) {
@@ -39,11 +57,8 @@ int main(int argc, const char* argv[]) {
         return -1;
     }
 
-    std::ostringstream os;
-    os << "{\"sensor_uuid\": \"" << argv[1] << "\", \"timestamp\": " << argv[2] << ", \"value\": " << argv[3] << "}";
 
-    jsonxx::Object measurement;
-    measurement.parse(os.str());
+    /* Create Dispatcher */
 
     WebsocketRails dispatcher(argv[4]);
     dispatcher.onOpen(boost::bind(on_open, _1));
@@ -52,7 +67,32 @@ int main(int argc, const char* argv[]) {
 
     dispatcher.connect();
 
+
+    /* Subscribe to Channel */
+
+    std::ostringstream channel_id;
+    channel_id << "sensor-" << argv[1];
+    std::cout << std::endl << "Subscribing channel: " << channel_id.str() << std::endl;
+
+    Channel channel = dispatcher.subscribe(channel_id.str(), boost::bind(subscription_success, _1), boost::bind(subscription_failure, _1));
+    channel.bind("create", boost::bind(received_measurement, _1));
+    channel.bind("update", boost::bind(received_measurement, _1));
+
+    std::cout << "Listening to channel: " << channel_id.str() << std::endl;
+
+
+    /* Post Measurements */
+
+    std::ostringstream measurement_str;
+    measurement_str << "{\"sensor_uuid\": \"" << argv[1] << "\", \"timestamp\": " << argv[2] << ", \"value\": " << argv[3] << "}";
+
+    jsonxx::Object measurement;
+    measurement.parse(measurement_str.str());
+
+    std::cout << std::endl << "Posting measurement: " << measurement_str.str() << std::endl;
+
     Event post = dispatcher.trigger("measurements.post", measurement, boost::bind(post_success, _1), boost::bind(post_failure, _1));
+
 
     /* Wait */
     char c;
